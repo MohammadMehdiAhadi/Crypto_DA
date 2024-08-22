4965aa4a776108a86c2b88b24a93abafeb25877c
Mohammad <mohammad.ahadi8282@gmail.com>
2024-08-22 12:22:15 +0330

    The one and only MAMAD

---
+++ Future/BTC_Behavior/btc_behavior.py
@@ -0,0 +1,34 @@
+
+try:
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+
+    print("Reading Data...")
+    df = pd.read_csv("C:/Crypto_DA/Future/Creating_Data/BTC_Price_15m.csv")
+    df.set_index(df["Datetime"], inplace=True)
+    print("Done")
+    print("Plotting...")
+
+    mean_btc = np.mean(df["Close"])
+    std_btc = np.std(df["Close"])
+    std_p = mean_btc + std_btc
+    std_n = mean_btc - std_btc
+    std2_p = mean_btc + (std_btc * 2)
+    std2_n = mean_btc - (std_btc * 2)
+
+    plt.plot(df.index, df["Close"], "r--", label="close")
+    plt.axhline(mean_btc, color='blue', linestyle='-', label="Mean")
+    plt.axhline(std_p, color='g', linestyle='--', label="Positive Std")
+    plt.axhline(std2_p, color='m', linestyle='--', label="Positive 2Std")
+    plt.axhline(std_n, color='y', linestyle='--', label="Negative Std")
+    plt.axhline(std2_n, color='c', linestyle='--', label="Negative 2Std")
+
+    plt.xticks(ticks=df["Datetime"].iloc[::500], rotation=45)
+    plt.legend()
+    print("Done")
+    plt.savefig("BTC_Behavior.jpg")
+    plt.show()
+except Exception as e:
+    print(e)
---
+++ Future/Creating_Data/make_data.py
@@ -0,0 +1,17 @@
+
+try:
+    import yfinance as yf
+
+    print("Downloading Data ...")
+    print("Please Wait")
+
+    df = yf.download("BTC-USD", period="60d", interval="15m")
+
+    if not df.empty:
+        df.to_csv("BTC_Price_15m.csv")
+        print("It's Done; Enjoy!")
+    else:
+        print("Check Your Connection please!")
+
+except Exception as e:
+    print(e)
